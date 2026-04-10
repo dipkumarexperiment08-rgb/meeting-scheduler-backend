a8808f4f60071a2e94a62f2bd646a25ae936e5fa
Dipkumar88 <satanideep07@gmail.com>
2026-04-10 10:10:56 +0530

    Add Socket.io real-time updates

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import 'express-async-errors'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 import connectDB from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
 import meetingRoutes from './routes/meetingRoutes.js'
@@ -11,6 +13,28 @@ dotenv.config()
 connectDB()
 
 const app = express()
+const httpServer = createServer(app)
+
+export const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id)
+
+  socket.on('join', (userId) => {
+    socket.join(userId)
+    console.log(`User ${userId} joined their room`)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id)
+  })
+})
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json())
@@ -21,6 +45,6 @@ app.use('/api/meetings', meetingRoutes)
 
 app.get('/', (req, res) => res.send('Meeting Scheduler API running'))
 
-app.listen(process.env.PORT, () => {
+httpServer.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
 })
